f4145ce2e7ffc8485900531297aa2225ce4721db
Deivid <Wesley@DESKTOP-AH5DODT>
2024-04-04 14:08:01 -0300

    Initial Commit

---
+++ src/main/java/br/sc/senac/tcs/TcsApplication.java
@@ -0,0 +1,13 @@
+package br.sc.senac.tcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcsApplication.class, args);
+	}
+
+}
