90933fef21de2a1003681a1cabc93349500cd25a
Francisco <alcionefranciscodeborba@gmail.com>
2024-04-16 16:18:57 -0300

    foi criada a classe ClienteService e todo o código da classe ClienteControllerfoi refatorado

--- src/main/java/br/sc/senac/tcs/controller/ClienteController.java
+++ src/main/java/br/sc/senac/tcs/controller/ClienteController.java
@@ -1,6 +1,5 @@
 package br.sc.senac.tcs.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,43 +12,39 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.sc.senac.tcs.model.entidade.Cliente;
-import br.sc.senac.tcs.repository.ClienteRepository;
+import br.sc.senac.tcs.service.ClienteService;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/api/clientes")
 public class ClienteController {
 
-    @Autowired
-    private ClienteRepository clienteRepository;
+    private final ClienteService clienteService;
 
-    @GetMapping("/list")
-    Iterable<Cliente> list() {
-        return clienteRepository.findAll();
+    @GetMapping
+    public Iterable<Cliente> list() {
+        return clienteService.findAll();
     }
 
     @GetMapping("{id}")
     public Cliente get(@PathVariable Integer id) {
-        return clienteRepository.findById(id).orElse(null);
+        return clienteService.findById(id);
     }
 
     @PostMapping
     public Cliente create(@RequestBody Cliente cliente) {
-        return clienteRepository.save(cliente);
+        return clienteService.create(cliente);
     }
 
     @PutMapping("{id}")
-    public Cliente update(@PathVariable Integer id, @RequestBody Cliente cliente) {
-        Cliente clienteDb = clienteRepository.findById(id).orElse(null);
-        clienteDb.setNome(cliente.getNome());
-        clienteDb.setBairro(cliente.getBairro());
-
-        return clienteRepository.save(clienteDb);
+    public Cliente update(@PathVariable Integer id, @RequestBody Cliente form) {
+        return clienteService.update(id, form);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("{id}")
     public void delete(@PathVariable Integer id) {
-        Cliente clienteDb = clienteRepository.findById(id).orElse(null);
-        clienteRepository.delete(clienteDb);
+        clienteService.delete(id);
     }
 }
--- src/main/java/br/sc/senac/tcs/service/ClienteService.java
+++ src/main/java/br/sc/senac/tcs/service/ClienteService.java
@@ -1,8 +1,40 @@
 package br.sc.senac.tcs.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.sc.senac.tcs.model.entidade.Cliente;
+import br.sc.senac.tcs.repository.ClienteRepository;
 
 @Service
 public class ClienteService {
-    
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public Iterable<Cliente> findAll() {
+        return clienteRepository.findAll();
+    }
+
+    public Cliente findById(Integer id) {
+        return clienteRepository.findById(id).orElse(null);
+    }
+
+    public Cliente create(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    public Cliente update(Integer id, Cliente form) {
+        Cliente clienteDb = findById(id);
+
+        clienteDb.setNome(form.getNome());
+        clienteDb.setEmail(form.getEmail());
+
+        return clienteRepository.save(clienteDb);
+    }
+
+    public void delete(Integer id) {
+        Cliente clienteDb = clienteRepository.findById(id).orElse(null);
+        clienteRepository.delete(clienteDb);
+    }
 }
