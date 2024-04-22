c5a30c8a1fc7408bb85588a832471ea82fb9aebb
Vitor Garcia <vitorgarciadevasconcelos@gmail.com>
2024-04-22 11:07:58 -0300

    feat: mudança em Sinistro
    Att dos scripts de create e insert do banco de dados

--- src/main/java/br/sc/senac/tcs/model/entidade/Corretor.java
+++ src/main/java/br/sc/senac/tcs/model/entidade/Corretor.java
@@ -6,27 +6,33 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "corretor")
+@NoArgsConstructor
 @Data
 public class Corretor {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    private String nome;
-    private String cpf;
-    private String ddd;
-    private String telefone;
-    private String email;
-    private String senha;
-    
-    // @JsonBackReference
-    // @OneToMany(mappedBy = "corretor")
-    // private List<Cliente> clientes;
-    
-    // @JsonBackReference
-    // @OneToMany(mappedBy = "corretor")
-    // private List<Seguro> seguros;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	private String cpf;
+	private String ddd;
+	private String telefone;
+	private String email;
+	private String senha;
+
+	public Corretor(Integer id, String nome, String cpf, String ddd, String telefone, String email, String senha) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.ddd = ddd;
+		this.telefone = telefone;
+		this.email = email;
+		this.senha = senha;
+	}
+
 }
