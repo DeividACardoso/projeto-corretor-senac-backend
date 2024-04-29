7d89eba6da3acccb398622e4eb954c2460d34f18
Francisco <alcionefranciscodeborba@gmail.com>
2024-04-29 09:09:30 -0300

    feat: cliente, seguro, veiculo

---
+++ src/main/java/br/sc/senac/tcs/entity/Cliente.java
@@ -0,0 +1,74 @@
+package br.sc.senac.tcs.entity;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@Table(name = "clientes")
+@EqualsAndHashCode(callSuper = true)
+public class Cliente extends Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    @Column(length = 255, nullable = false)
+    private int id;
+
+    @Column(length = 255, nullable = false)
+    private String cnh;
+
+    @Column(length = 255, nullable = false)
+    private String ddd;
+
+    @Column(length = 255, nullable = false)
+    private String telefone;
+
+    @Column(nullable = false)
+    private int idEndereco;
+
+    @Column(length = 255, nullable = false)
+    private String estadoCivil;
+
+    @Column(length = 255, nullable = false)
+    private String genero;
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Seguro> seguros;
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Veiculo> veiculos;
+
+    public String getCnh() {
+        return this.cnh;
+    }
+
+    @Override
+    public void operation1() {
+        throw new UnsupportedOperationException("Unimplemented method 'operation1'");
+    }
+
+    @Override
+    protected void operation2() {
+        throw new UnsupportedOperationException("Unimplemented method 'operation2'");
+    }
+
+    @Override
+    protected void operation3() {
+        throw new UnsupportedOperationException("Unimplemented method 'operation3'");
+    }
+
+    @Override
+    public Long getId() {
+        return (long) this.id;
+    }
+
+}
