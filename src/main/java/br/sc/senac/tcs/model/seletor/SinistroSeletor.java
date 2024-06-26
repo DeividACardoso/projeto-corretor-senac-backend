8c8ea7d5c9e858795261dd35a17523b2f73a9777
vitorGarcia <vitorgarciadevasconcelos@gmail.com>
2024-06-26 15:16:24 -0300

    refactor: atualizando o Sinistro seletor

--- src/main/java/br/sc/senac/tcs/model/seletor/SinistroSeletor.java
+++ src/main/java/br/sc/senac/tcs/model/seletor/SinistroSeletor.java
@@ -1,6 +1,8 @@
 package br.sc.senac.tcs.model.seletor;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 import jakarta.persistence.Column;
 
@@ -8,70 +10,15 @@ public class SinistroSeletor extends BaseSeletor {
 
 	private Integer id;
 	private String tipo;
-	@Column(name = "DT_HORA")
+	@Column(name = "data")
 	private LocalDateTime dataInicial;
-	@Column(name = "DT_HORA")
+	@Column(name = "data")
 	private LocalDateTime dataFinal;
-	@Column(name = "DT_HORA")
+	@Column(name = "horario")
 	private LocalDateTime horarioInicial;
-	@Column(name = "DT_HORA")
+	@Column(name = "horario")
 	private LocalDateTime horarioFinal;
 	private String descricao;
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getTipo() {
-		return tipo;
-	}
-
-	public void setTipo(String tipo) {
-		this.tipo = tipo;
-	}
-
-	public LocalDateTime getDataInicial() {
-		return dataInicial;
-	}
-
-	public void setDataInicial(LocalDateTime dataInicial) {
-		this.dataInicial = dataInicial;
-	}
-
-	public LocalDateTime getDataFinal() {
-		return dataFinal;
-	}
-
-	public void setDataFinal(LocalDateTime dataFinal) {
-		this.dataFinal = dataFinal;
-	}
-
-	public LocalDateTime getHorarioInicial() {
-		return horarioInicial;
-	}
-
-	public void setHorarioInicial(LocalDateTime horarioInicial) {
-		this.horarioInicial = horarioInicial;
-	}
-
-	public LocalDateTime getHorarioFinal() {
-		return horarioFinal;
-	}
-
-	public void setHorarioFinal(LocalDateTime horarioFinal) {
-		this.horarioFinal = horarioFinal;
-	}
-
-	public String getDescricao() {
-		return descricao;
-	}
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
+	
 
 }
