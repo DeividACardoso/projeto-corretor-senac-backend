f9749fdfb85039224750c5c22fb28471083ba2bc
Vitor Garcia0 <vitorgarciadevasconcelos@gmail.com>
2024-07-08 09:04:55 -0300

    refactor: pesquisa de Sinistro

--- src/main/java/br/sc/senac/tcs/model/specification/SinistroSpecification.java
+++ src/main/java/br/sc/senac/tcs/model/specification/SinistroSpecification.java
@@ -16,20 +16,30 @@ public class SinistroSpecification {
 			List<Predicate> predicates = new ArrayList<>();
 
 			if (seletor.getTipo() != null && !seletor.getTipo().trim().isEmpty()) {
-			      predicates.add(cb.equal(root.get("tipo"), seletor.getTipo().toLowerCase()));
-			    }
+				predicates.add(cb.equal(root.get("tipo"), seletor.getTipo().toLowerCase()));
+			}
 
-			    if (seletor.getDataInicial() != null && seletor.getDataFinal() != null) {
-			      predicates.add(cb.between(root.get("dataInicial"), seletor.getDataInicial(), seletor.getDataFinal()));
-			    }
+			if (seletor.getDataInicial() != null && seletor.getDataFinal() != null) {
+				predicates.add(cb.between(root.get("dataInicial"), seletor.getDataInicial(), seletor.getDataFinal()));
+			} else if (seletor.getDataInicial() != null) {
+				predicates.add(cb.greaterThanOrEqualTo(root.get("dataInicial"), seletor.getDataInicial()));
+			} else if (seletor.getDataFinal() != null) {
+				predicates.add(cb.lessThanOrEqualTo(root.get("dataInicial"), seletor.getDataFinal()));
+			}
 
-			    if (seletor.getHorarioInicial() != null && seletor.getHorarioFinal() != null) {
-			      predicates.add(cb.between(root.get("horarioInicial"), seletor.getHorarioInicial(), seletor.getHorarioFinal()));
-			    }
+			if (seletor.getHorarioInicial() != null && seletor.getHorarioFinal() != null) {
+				predicates.add(
+						cb.between(root.get("horarioInicial"), seletor.getHorarioInicial(), seletor.getHorarioFinal()));
+			} else if (seletor.getHorarioInicial() != null) {
+				predicates.add(cb.greaterThanOrEqualTo(root.get("horarioInicial"), seletor.getHorarioInicial()));
+			} else if (seletor.getHorarioFinal() != null) {
+				predicates.add(cb.lessThanOrEqualTo(root.get("horarioInicial"), seletor.getHorarioFinal()));
+			}
 
-			    if (seletor.getDescricao() != null && !seletor.getDescricao().trim().isEmpty()) {
-			      predicates.add(cb.like(root.get("descricao"), "%" + seletor.getDescricao().toLowerCase() + "%"));
-			    }
+			if (seletor.getDescricao() != null && !seletor.getDescricao().trim().isEmpty()) {
+				predicates.add(cb.like(cb.lower(root.get("descricao")),
+						"%" + seletor.getDescricao().trim().toLowerCase() + "%"));
+			}
 
 			return cb.and(predicates.toArray(new Predicate[0]));
 
